1e3fe85c737ddb4bdc7b2b86886c5c6527edb2b9
jjingbak <qurdoo228@gmail.com>
2019-06-28 14:51:07 +0900

    멤버

---
+++ src/main/java/org/ks/member/VO/Member.java
@@ -0,0 +1,87 @@
+package org.ks.member.VO;
+
+public class Member {
+	private String id;
+	private String pw;
+	private String name;
+	private String nickname;
+	private String gender;
+	private String add1;
+	private String addr2;
+	private String phone;
+	private String memberImg;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getAdd1() {
+		return add1;
+	}
+	public void setAdd1(String add1) {
+		this.add1 = add1;
+	}
+	public String getAddr2() {
+		return addr2;
+	}
+	public void setAddr2(String addr2) {
+		this.addr2 = addr2;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getMemberImg() {
+		return memberImg;
+	}
+	public void setMemberImg(String memberImg) {
+		this.memberImg = memberImg;
+	}
+	public Member(String id, String pw, String name, String nickname, String gender, String add1, String addr2,
+			String phone, String memberImg) {
+		super();
+		this.id = id;
+		this.pw = pw;
+		this.name = name;
+		this.nickname = nickname;
+		this.gender = gender;
+		this.add1 = add1;
+		this.addr2 = addr2;
+		this.phone = phone;
+		this.memberImg = memberImg;
+	}
+	public Member() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
